7b1022f60b3e803345aa3d492a7230b0a4cca647
amani bouzenzana <bouzenzana.amani@gmail.com>
2024-12-16 11:49:17 +0000

    Coding Challenge: Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,22 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1; 
+        }
+
+        int a = 0; 
+        int b = 1; 
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            result = a + b; 
+            a = b; 
+            b = result;
+        }
+
+        return result;
     }
 }
